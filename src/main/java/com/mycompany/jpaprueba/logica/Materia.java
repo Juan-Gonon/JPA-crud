0de4b371e2f984b7a7022e27779e038a54bb05c7
Juan Gonon <juanfegonon27@gmail.com>
2025-01-09 21:47:52 +0000

    Materia

--- src/main/java/com/mycompany/jpaprueba/logica/Materia.java
+++ src/main/java/com/mycompany/jpaprueba/logica/Materia.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.jpaprueba.logica;
 
-/**
- *
- * @author jgono
- */
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.GenerationType;
+
+@Entity
 public class Materia {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+
+    @Basic
+    private String tipo;
+
+    public Materia() {
+    }
+
+    public Materia(int id, String tipo) {
+        this.id = id;
+        this.tipo = tipo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
 }
