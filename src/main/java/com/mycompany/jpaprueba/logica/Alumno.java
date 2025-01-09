5ad038f25b2ebcc6b83874987e374845966d4bfd
Juan Gonon <juanfegonon27@gmail.com>
2025-01-09 17:30:19 +0000

    relacion OneToOne

--- src/main/java/com/mycompany/jpaprueba/logica/Alumno.java
+++ src/main/java/com/mycompany/jpaprueba/logica/Alumno.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -23,6 +24,9 @@ public class Alumno implements Serializable {
     
     @Temporal(TemporalType.DATE)
     private Date fechaNac;
+    
+    @OneToOne
+    private Carrera carrera;
 
     @Override
     public String toString() {
@@ -32,13 +36,22 @@ public class Alumno implements Serializable {
     public Alumno() {
     }
 
-    public Alumno(int id, String nombre, String apellido, Date fechaNac) {
+    public Alumno(int id, String nombre, String apellido, Date fechaNac, Carrera carrera) {
         this.id = id;
         this.nombre = nombre;
         this.apellido = apellido;
         this.fechaNac = fechaNac;
+        this.carrera = carrera;
     }
 
+    public Carrera getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(Carrera carrera) {
+        this.carrera = carrera;
+    }
+    
     public int getId() {
         return id;
     }
